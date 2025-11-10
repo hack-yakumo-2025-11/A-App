6c0bdd7d252fe8e7b254798e45f048a4869e453b
LEITE KOTA THIAGO YUZO <thiago_yuzo@toagroup.local>
2025-11-10 14:09:21 +0900

    Store pages

---
+++ src/pages/Map.jsx
@@ -0,0 +1,10 @@
+import { Box, Container, Heading, Text } from '@chakra-ui/react';
+
+export default function Map() {
+  return (
+    <Container maxW="container.md" py={8}>
+      <Heading>Map 🗺️</Heading>
+      <Text mt={4}>Coming in Day 2!</Text>
+    </Container>
+  );
+}
